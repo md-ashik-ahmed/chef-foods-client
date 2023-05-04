571ec6ef92f1b5314f72e13526bc7011993a2173
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-05-04 11:32:56 +0600

    added Extra Section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Card from '../Card/Card';
 import { AuthContext } from '../Provider/AuthProvider';
 import Banner from '../Banner/Banner';
 import Popular from '../ExtraSection/Popular';
+import Message from '../ExtraSection/Message';
 
 
 const Home = () => {
@@ -23,6 +24,7 @@ const Home = () => {
 
         <div>
           <div className='px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'> <Banner></Banner></div>
+          <div className='my-container'><Message></Message></div>
           <div className='my-container'>
          <h1 className='font-sans font-bold text-6xl text-center pb-16'>Meet Our Chef's</h1>
        <div className='grid lg:grid-cols-3 gap-8'>
